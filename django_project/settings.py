a40dae75fcbddf4bd8330dda6bd78022cf7c87c9
iremarcha <iremarcha@alum.us.es>
2020-01-23 00:01:40 +0100

    v8

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,9 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#v6
+
+#v8
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
-
 LOGIN_URL = 'login'
