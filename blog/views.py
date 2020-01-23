42a5e44ddcf609d454e79bb8f79fa43a28745cc2
iremarcha <iremarcha@alum.us.es>
2020-01-23 19:50:48 +0100

    v11

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
-#v10
+from django.contrib.auth.models import User
 from django.views.generic import (ListView, 
 								  DetailView,
 								  CreateView,
 								  UpdateView,
 								  DeleteView)
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-# v2
-#def home(request):
-#	return HttpResponse('<h1>Blog Home</h1>')
 
-#def about(request):
-#	return HttpResponse('<h1>About</h1>')
 
-#v3
 posts = [
 	{
 		'author': 'CoreyMS',
@@ -44,6 +38,17 @@ class PostListView(ListView):
 	template_name = 'blog/home.html'
 	context_object_name = 'posts'
 	ordering = ['-date_posted']
+	paginate_by = 5
+
+class UserPostListView(ListView):
+	model = Post
+	template_name = 'blog/user_posts.html'
+	context_object_name = 'posts'
+	paginate_by = 5
+
+	def get_queryset(self):
+		user = get_object_or_404(User, username =self.kwargs.get('username'))
+		return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
 	model = Post
